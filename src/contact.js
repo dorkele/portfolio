12f42e7fb1a22e03eb2c9574635e691b5c95e985
dorkele <dora_tominic@hotmail.com>
2020-10-05 15:54:01 +0200

    CV

--- src/contact.js
+++ src/contact.js
@@ -1,9 +1,11 @@
 import React from "react";
 import styles from "./contact.module.css";
+import pdf from "./documents/d.tominic-cv.pdf";
 
 export default function Contact() {
     return (
         <div id="contact" className={styles.container}>
+            <p>dora_tominic@hotmail.com</p>
             <a
                 target="_blank"
                 rel="noopener noreferrer"
@@ -11,13 +13,15 @@ export default function Contact() {
             >
                 Github
             </a>
-            <p>dora_tominic@hotmail.com</p>
             <a
                 target="_blank"
                 rel="noopener noreferrer"
                 href="https://www.linkedin.com/in/dora-tominic/"
             >
                 Linkedin
+            </a>
+            <a target="_blank" rel="noopener noreferrer" href={pdf}>
+                Resume
             </a>
         </div>
     );
