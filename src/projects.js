3318b7574303726fb9a3d4c9e3bf52f2269a42a0
dora <dora.tominic@sideos.io>
2023-06-03 20:12:28 +0200

    removed unused code

--- src/projects.js
+++ src/projects.js
@@ -1,20 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./projects.module.css";
-import Project from "./project.js";
 
 export default function Projects() {
-    let [id, setId] = useState("");
-    let [visible, setVisible] = useState(false);
-    const [count, setCount] = useState(0);
 
-    function openModal(id) {
-        setCount(0);
-        setId(id);
-        setVisible(true);
-    }
-    function closeModal() {
-        setVisible(false);
-    }
     return (
         <React.Fragment>
             <div id="projects" className={styles.container}>
@@ -24,16 +12,6 @@ export default function Projects() {
                     Coming Soon!
                 </p>
             </div>
-            {id && (
-                <Project
-                    id={id}
-                    closeModal={closeModal}
-                    visible={visible}
-                    setVisible={setVisible}
-                    count={count}
-                    setCount={setCount}
-                />
-            )}
         </React.Fragment>
     );
 }
